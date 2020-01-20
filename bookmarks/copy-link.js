2bc8f1a3f8566b9d0258ad4ab992f4e1ef4f45e4
Jonathan Dror <jon@dror.io>
2020-01-20 10:10:00 -0600

    copy-link bookmark

---
+++ bookmarks/copy-link.js
@@ -0,0 +1,17 @@
+javascript: function textToClipboard(text) {
+    var dummy = document.createElement("a");
+    dummy.innerHTML = text;
+    dummy.href = window.location.href;
+    document.body.appendChild(dummy);
+    const range = document.createRange();
+    range.selectNode(dummy);
+    const selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    const successful = document.execCommand('copy');
+    document.body.removeChild(dummy);
+}
+url = window.location.href;
+num = url.substr(url.lastIndexOf('/') + 1);
+textToClipboard(num);
